873f925f255aa4cb7417491792f8189cc63ca90f
Tommy Allen <tommy@esdf.io>
2016-07-05 00:41:48 -0400

    Prioritizing completions in deoplete.nvim

--- rplugin/python3/deoplete/sources/django.py
+++ rplugin/python3/deoplete/sources/django.py
@@ -1,4 +1,16 @@
+import re
+
 from .base import Base
+
+
+_high_priority_re = re.compile(
+    r'(?:'
+    r'(?:\.objects\.\w*)|'
+    r'(?:\bsettings\.\w*)|'
+    r'(?:\{%\s+(?:include|extends)\s+["\'][\w/]+)|'
+    r'(?:(?:render\([^,]+,|get_template\(|render_to_string\(|render_to_response\(|template_name\s*=)\s*["\'][\w/]*)'
+    r')$'
+)
 
 
 class Source(Base):
@@ -10,8 +22,13 @@ class Source(Base):
         self.input_pattern = '.\.\w*$|\|\w*$|\{%\s+\w*$'
 
     def get_complete_position(self, context):
+        self.rank = 10
+        line = context.get('input')
+        if line and _high_priority_re.search(line):
+            self.rank = 9000
         return self.vim.call('djangoplus#complete', 1, '')
 
     def gather_candidates(self, context):
-        return self.vim.call('djangoplus#complete', 0,
-                             context['complete_str'])
+        candidates = self.vim.call('djangoplus#complete', 0,
+                                   context['complete_str'])
+        return candidates
